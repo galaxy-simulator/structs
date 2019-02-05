8e00d33dbba2d645065ee99b681888ccd63a7e9a
Emile <hanemile@protonmail.com>
2019-02-05 21:22:37 +0100

    adjusted the comments to godoc standard

--- vector2D.go
+++ vector2D.go
@@ -18,12 +18,12 @@ func (v *Vec2) Copy() Vec2 {
 	return Vec2{v.X, v.Y}
 }
 
-// returns the product of the vector and a scalar s
+// Multiply returns the product of the vector and a scalar s
 func (v *Vec2) Multiply(s float64) Vec2 {
 	return Vec2{v.X * s, v.Y * s}
 }
 
-// returns the sum of this vector and the vector v2
+// Add returns the sum of this vector and the vector v2
 func (v1 *Vec2) Add(v2 Vec2) Vec2 {
 	return Vec2{v1.X + v2.X, v1.Y + v2.Y}
 }
